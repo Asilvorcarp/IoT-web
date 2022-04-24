f648d638e8159f393003d1a40ad2c5fa0a3edb29
Asilvorcarp <asilvorcarp@qq.com>
2022-04-24 11:26:16 +0800

    aaaa

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/fancy_button.scss'
+import '../styles/fancy_input.scss'
+import '../styles/Home.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
